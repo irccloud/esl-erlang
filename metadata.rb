394d7bc41f53297d3fa66fe7131042ce7cf815a2
Richard Jones <rj@metabrew.com>
2014-10-10 13:19:53 +0100

    remove apt dep version lock

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ license          "All rights reserved"
 description      "Installs/Configures esl-erlang"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "99.0.1"
-depends          "apt", "1.7.0"
+depends          "apt"
