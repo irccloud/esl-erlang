e55ec279a040807ecff34607970498b3a8b10946
Richard Jones <rj@metabrew.com>
2015-01-12 16:32:27 +0000

    version bump 99.0.4

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "YOUR_EMAIL"
 license          "All rights reserved"
 description      "Installs/Configures esl-erlang"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "99.0.3"
+version          "99.0.4"
 depends          "apt"
